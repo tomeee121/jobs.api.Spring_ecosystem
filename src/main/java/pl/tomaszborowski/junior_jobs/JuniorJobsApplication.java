700541fbce3b92d3dd8d48f48d3fd6a0f9c9de7b
Tomasz Borowski <bortom112233@gmail.com>
2022-08-10 12:20:16 +0200

    init commit

---
+++ src/main/java/pl/tomaszborowski/junior_jobs/JuniorJobsApplication.java
@@ -0,0 +1,13 @@
+package pl.tomaszborowski.junior_jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniorJobsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuniorJobsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/pl/tomaszborowski/junior_jobs/config/Configuration.java
@@ -0,0 +1,4 @@
+package pl.tomaszborowski.junior_jobs.config;
+
+public class Configuration {
+}

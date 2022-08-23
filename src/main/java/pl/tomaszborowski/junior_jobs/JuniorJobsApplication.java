a505f307ccad73dd4bfa885c436879f9de643387
Tomasz Borowski <bortom112233@gmail.com>
2022-08-23 08:35:01 +0200

    Dependencies refactor

--- src/main/java/pl/tomaszborowski/junior_jobs/JuniorJobsApplication.java
+++ src/main/java/pl/tomaszborowski/junior_jobs/JuniorJobsApplication.java
@@ -3,9 +3,10 @@ package pl.tomaszborowski.junior_jobs;
 import com.github.cloudyrock.spring.v5.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @EnableMongock
-@SpringBootApplication
+@SpringBootApplication(exclude = MongoAutoConfiguration.class)
 public class JuniorJobsApplication {
 
     public static void main(String[] args) {

f14cfe0aef0bad3456f788b12fd4edfc712abe87
Tomasz Borowski <bortom112233@gmail.com>
2022-08-29 09:53:38 +0200

    added configuration for container-based integration method class

--- src/test/java/pl/tomaszborowski/junior_jobs/offer/domain/OfferServiceWithContainerTest.java
+++ src/test/java/pl/tomaszborowski/junior_jobs/offer/domain/OfferServiceWithContainerTest.java
@@ -1,4 +1,26 @@
 package pl.tomaszborowski.junior_jobs.offer.domain;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import pl.tomaszborowski.junior_jobs.JuniorJobsApplication;
+
+@SpringBootTest(classes = JuniorJobsApplication.class)
+@Profile("container")
+@Testcontainers
 public class OfferServiceWithContainerTest {
+
+    @Container
+    private final static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo");
+
+    static {
+        mongoDBContainer.start();
+        Integer port = mongoDBContainer.getFirstMappedPort();
+        System.setProperty("DB_PORT", String.valueOf(port));
+    }
+
+
 }

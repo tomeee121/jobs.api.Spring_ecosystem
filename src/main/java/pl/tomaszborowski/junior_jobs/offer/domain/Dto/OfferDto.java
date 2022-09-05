1af768ca16d190423a8520f15531e91fe342761f
Tomasz Borowski <bortom112233@gmail.com>
2022-09-05 15:28:39 +0200

    Added validation for Dto offers

--- src/main/java/pl/tomaszborowski/junior_jobs/offer/domain/Dto/OfferDto.java
+++ src/main/java/pl/tomaszborowski/junior_jobs/offer/domain/Dto/OfferDto.java
@@ -3,6 +3,9 @@ package pl.tomaszborowski.junior_jobs.offer.domain.Dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,12 +16,20 @@ public class OfferDto {
     @JsonProperty("id")
     private String id;
     @JsonProperty("company")
+    @NotNull(message = "Offer company name cannot be null.")
+    @NotEmpty(message = "Offer company name cannot be empty.")
     private String companyName;
     @JsonProperty("position")
+    @NotNull(message = "Offer position cannot be null.")
+    @NotEmpty(message = "Offer position cannot be empty.")
     private String position;
     @JsonProperty("salary")
+    @NotNull(message = "Offer salary cannot be null.")
+    @NotEmpty(message = "Offer salary cannot be empty.")
     private String salary;
     @JsonProperty("offerUrl")
+    @NotNull(message = "Offer url cannot be null.")
+    @NotEmpty(message = "Offer url cannot be empty.")
     private String offerUrl;
 }
 

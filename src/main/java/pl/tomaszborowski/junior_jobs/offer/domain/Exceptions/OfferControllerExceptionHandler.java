410e774d613e7113dedcd70854a65fe9d4d6167c
Tomasz Borowski <bortom112233@gmail.com>
2022-09-05 15:40:17 +0200

    Added exception handling for passing wrong data at POST /offers endpoint

--- src/main/java/pl/tomaszborowski/junior_jobs/offer/domain/Exceptions/OfferControllerExceptionHandler.java
+++ src/main/java/pl/tomaszborowski/junior_jobs/offer/domain/Exceptions/OfferControllerExceptionHandler.java
@@ -3,10 +3,14 @@ package pl.tomaszborowski.junior_jobs.offer.domain.Exceptions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
 
 @ControllerAdvice
 @Slf4j
@@ -26,6 +30,18 @@ public class OfferControllerExceptionHandler {
     @ResponseStatus(HttpStatus.CONFLICT)
     public ResponseEntity<OfferErrorResponse> offerExistsExceptionResponse(OfferExistsException exception){
         OfferErrorResponse offerErrorResponse = new OfferErrorResponse(exception.getMessage(), HttpStatus.CONFLICT);
+        log.info("Exception thrown with message: "+offerErrorResponse);
+
+        return new ResponseEntity<>(offerErrorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<OfferErrorResponse> constraintViolationExceptionResponse(MethodArgumentNotValidException exception){
+        ArrayList<String> exceptions = new ArrayList<>();
+        exception.getBindingResult().getAllErrors().forEach(exceptionOccured -> exceptions.add(exceptionOccured.toString()));
+        OfferErrorResponse offerErrorResponse = new OfferErrorResponse(exceptions.toString(), HttpStatus.CONFLICT);
         log.info("Exception thrown with message: "+offerErrorResponse);
 
         return new ResponseEntity<>(offerErrorResponse, HttpStatus.NOT_FOUND);

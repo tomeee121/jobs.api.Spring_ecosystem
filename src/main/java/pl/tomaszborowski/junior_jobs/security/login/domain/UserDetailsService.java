81b20e2a20a6f4b8c70f0575f1453872529bf3a1
Tomasz Borowski <bortom112233@gmail.com>
2022-09-06 15:13:11 +0200

    Implemented UserDetailsService

--- src/main/java/pl/tomaszborowski/junior_jobs/security/login/domain/UserDetailsService.java
+++ src/main/java/pl/tomaszborowski/junior_jobs/security/login/domain/UserDetailsService.java
@@ -1,4 +1,25 @@
 package pl.tomaszborowski.junior_jobs.security.login.domain;
 
-public class UserDetailsService {
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import pl.tomaszborowski.junior_jobs.security.login.domain.Dao.User;
+import pl.tomaszborowski.junior_jobs.security.login.domain.Dao.UserRepo;
+
+import java.util.Collections;
+
+@RequiredArgsConstructor
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    private final UserRepo userRepo;
+    private static final String USER_NOT_FOUND = "User not found";
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findUserByUsername(username).orElseThrow(() -> new UsernameNotFoundException(USER_NOT_FOUND));
+        UserDetails userDetails = new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), Collections.emptyList());
+        return userDetails;
+
+    }
 }
